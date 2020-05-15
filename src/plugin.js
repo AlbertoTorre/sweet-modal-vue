1252ee03475eb5a212c774ef13316b4c6710debb
dehost <albertochoatorres@gmail.com>
2020-05-15 13:41:26 -0500

    change plugin export

--- src/plugin.js
+++ src/plugin.js
@@ -1,6 +1,29 @@
 import { SweetModal, SweetModalTab } from './main.js'
 
-export default function install(Vue, options) {
-	Vue.component('SweetModal', SweetModal)
-	Vue.component('SweetModalTab', SweetModalTab)
+function install(Vue) {
+
+  if (install.installed) return;
+  install.installed = true;
+
+  Vue.component('SweetModal', SweetModal)
+  Vue.component('SweetModalTab', SweetModalTab)
 }
+
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+SweetModal.install = install;
+
+export default { SweetModal, SweetModalTab } ;
